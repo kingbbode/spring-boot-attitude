636465659cb2837846897771028953ba999450ed
kingbbode <kingbbode@gmail.com>
2017-02-28 21:45:59 +0900

    create boot!

---
+++ src/main/java/com/kingbbode/SpringBootAttitudeApplication.java
@@ -0,0 +1,12 @@
+package com.kingbbode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAttitudeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAttitudeApplication.class, args);
+	}
+}
