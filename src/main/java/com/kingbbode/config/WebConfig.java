fd099bc5042bc8bb672580d56d0dc35746c5dd8d
kingbbode <kingbbode@gmail.com>
2017-03-22 23:46:07 +0900

    jackson remove - spring boot 1.3~ default - https://github.com/spring-projects/spring-boot/wiki/spring-boot-1.3-release-notes#jackson

--- src/main/java/com/kingbbode/config/WebConfig.java
+++ src/main/java/com/kingbbode/config/WebConfig.java
@@ -1,34 +1,16 @@
 package com.kingbbode.config;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.ViewResolver;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
 import org.springframework.web.servlet.view.freemarker.FreeMarkerViewResolver;
-
-import java.util.List;
 
 /**
  * Created by YG_king on 2017-03-07.
  */
-@EnableWebMvc
 @Configuration
-public class WebConfig extends WebMvcConfigurerAdapter {
-    @Override
-    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
-        final MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
-        final ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
-        converter.setObjectMapper(objectMapper);
-        converters.add(converter);
-        super.configureMessageConverters(converters);
-    }
+public class WebConfig {
 
     @Bean
     public ViewResolver viewResolver() {
